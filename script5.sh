ec518957edf55808662a10477b47f56b49069f88
adityaraj15325127 <aditya.24mip10040@vitbhopal.ac.in>
2026-03-31 21:22:08 +0530

    script5.sh

--- script5.sh
+++ script5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script 5: Open Source Manifesto Generator
-# Author: Aryawart Gupta
+# Author: Aditya Raj
 # Course: Open Source Software
 
 echo "========================================="
